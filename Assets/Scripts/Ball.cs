203fbd55812d924ebdafef705887723d9ce5c5fe
darwin <darwinsanluisramos21@gmail.com>
2023-03-12 02:13:26 +0800

    added physics material 2d

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,22 +6,16 @@ public class Ball : MonoBehaviour
 {
     public float ballForce = 1f;
     public Rigidbody2D rb;
-    // Start is called before the first frame update
-
 
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
 
-        int random1 = Random.Range(-5, 5);
-        int random2 = Random.Range(-5, 5);
+        float random1 = Random.Range(0, 2) == 0 ? -1:1;
+        int random2 = Random.Range(0, 2) == 0 ? -1:1;
         rb = GetComponent<Rigidbody2D>();
         
-       rb.AddForce(new Vector2(1, random2).normalized * ballForce, ForceMode2D.Impulse);
+        rb.AddForce(new Vector2(1, random2).normalized * ballForce, ForceMode2D.Impulse);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
 }
--- Assets/Scripts/PaddleA.cs
+++ Assets/Scripts/PaddleA.cs
@@ -7,18 +7,10 @@ public class PaddleA : MonoBehaviour
 
     public float paddleSpeed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.W)) {
             transform.position += new Vector3(0, 1 * paddleSpeed * Time.deltaTime);
-            //transform.position = transform.position + new Vector2(0, 1) * Time.deltaTime * paddleSpeed;
         }
         else if (Input.GetKey(KeyCode.S))
         {
