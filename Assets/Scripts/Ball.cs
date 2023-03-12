d927c372915c7a92618bcddd30e41ae51b15fc22
darwin <darwinsanluisramos21@gmail.com>
2023-03-12 16:53:54 +0800

    change condition

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float ballForce = 1f;
+    public float ballForce = 2f;
     public Rigidbody2D rb;
 
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
 
-        float random1 = Random.Range(0, 2) == 0 ? -1:1;
-        int random2 = Random.Range(0, 2) == 0 ? -1:1;
+        float random1 = Random.Range(-5, 5); 
+        int random2 = Random.Range(-2, 2);
+
+        if(random1 == 0)
+            random1 = 2;
+        if (random2 == 0)
+            random2 = 2;
+
         rb = GetComponent<Rigidbody2D>();
-        
         rb.AddForce(new Vector2(random1, random2).normalized * ballForce, ForceMode2D.Impulse);
     }
 
